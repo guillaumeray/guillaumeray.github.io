7397abec63d6183ca97749dfb3ef579f133066c5
Guillaume Rayes <guillaume.rayes@hermes.com>
2026-03-20 11:42:18 +0100

    Install Chirpy theme

--- assets/custom/js/fn.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    let dateString = document.querySelector('#age').getAttribute('value');
-    var ageInMilliseconds = new Date() - new Date(dateString);
-    document.querySelector('#age').innerHTML = Math.floor(ageInMilliseconds/1000/60/60/24/365); // convert to years
-});
